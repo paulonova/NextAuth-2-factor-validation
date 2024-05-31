d58a3e08109153d02197f237a99b916896f061ba
paulonova <paulonova144@gmail.com>
2024-05-31 10:38:32 +0200

    Fixing middleware

---
+++ auth.ts
@@ -0,0 +1,10 @@
+import NextAuth from 'next-auth'
+import GitHub from 'next-auth/providers/github'
+import Google from 'next-auth/providers/google'
+
+export const {
+  handlers: { GET, POST },
+  auth,
+} = NextAuth({
+  providers: [GitHub, Google],
+})
